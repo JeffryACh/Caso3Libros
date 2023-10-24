ad53be1cc995bd38d875751946db61652c2f9339
Jeffry Araya Ch <72415766+JeffryACh@users.noreply.github.com>
2023-10-24 10:39:49 -0600

    Makefile

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * Caso 3
+ * Autores:
+ *  - Jeffry Araya Ch (2022437443)
+ *  - Andres Picado Q 
+ * Fecha: 23/10/2023 15:50
+ * Ultima modificacion: 
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    return 0;
+}
