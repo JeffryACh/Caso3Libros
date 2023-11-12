17a8043673d37c8baa59940694e56731cdf7aab5
andresquipicadoo <aquirosp592@gmail.com>
2023-11-11 22:24:15 -0600

    Cambios al main
    Se usa una clase diferente para no interferir de momento con el main existente.
    
    El cambio principal es mostrar los documentos según el ranking y los tres mejores párrafos para cada documento.
    
    Pendiente: cortar el despliegue de cada párrafo cuando es muy grande ...

--- MainTFIDF.cpp
+++ MainTFIDF.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 #include "IndexadorTFIDF.h"
+
+void imprimirIndentado(const std::string& texto, int nivelIndentado) {
+    std::istringstream iss(texto);
+    std::string line;
+    std::string indent(nivelIndentado, ' ');
+
+    while (std::getline(iss, line)) {
+        std::cout << indent << line << std::endl;
+    }
+}
 
 int main(int argc, char **argv) {
     IndexadorLibros indexador = IndexadorLibros();
@@ -8,6 +20,7 @@ int main(int argc, char **argv) {
         bool salir = false;
         while (!salir) {
             std::string consulta;
+            std::cout << "*************************************************************" << std::endl;
             std::cout << "Ingrese la consulta a buscar (\"salir\" termina el programa): ";
             std::getline(std::cin, consulta);
             if (consulta == "salir") {
@@ -18,11 +31,31 @@ int main(int argc, char **argv) {
             }
             else {
                 cout << "Buscando " << consulta << endl;
-                vector<pair<int, float>> resultado = indexador.buscar(consulta, INDEXADOR_OR);
+                vector<pair<int, double>> resultado = indexador.buscar(consulta);
                 if (resultado.size() > 0) {
+                    int cant = 0;
+                    std::cout << "Se encontraron " << resultado.size() << " coincidencias de un total de "
+                              << indexador.cantidadDocumentos() << " libros" << std::endl;
                     std::cout << "Libros de referencia: " << std::endl;
                     for (auto &doc_id : resultado) {
-                        std::cout << "Archivo: " << indexador.getDocumento(doc_id.first) << ". Score:  " << doc_id.second << std::endl;
+                        Documento& doc = indexador.getDocumento(doc_id.first);
+                        double rank = doc_id.second < 0.0 ? 0.0 : doc_id.second;
+                        std::cout << "Libro: " << doc.getTitulo() << "(" << doc.getAutor() << "). Ranking:  "
+                                      << std::fixed << std::setprecision(8) << rank << std::endl;
+                        if (rank > 0.0) {
+                            cant++;
+                        }
+                        vector<pair<int, int>> parrafos = indexador.obtenerParrafosRelevantes(consulta, doc_id.first);
+                        if (!parrafos.empty()) {
+                            std::cout << ">>>> Parrafos relevantes <<<<" << std::endl;
+                        }
+                        for (auto &parrafo_id : parrafos) {
+                            std::cout << "<<< Parrafo #" << parrafo_id.first + 1 << " >>> " << std::endl;
+                            imprimirIndentado(indexador.textoParrafo(doc_id.first, parrafo_id.first), 4);
+                        }
+                    }
+                    if (cant == 0) {
+                        std::cout << "Trate de especificar una consulta con más palabras para resultados más relevantes " << std::endl;
                     }
                 } else {
                     std::cout << "No se encontraron libros de referencia para las palabras buscadas" << std::endl;
